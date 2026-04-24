5cd89e8b5fde10adbacb2f20ad0ff1530f97a7be
jojo <jojositohangg@gmail.com>
2026-04-24 11:11:54 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const databaseProyek = {
+    "1": {
+        judul: "Arthur Morgan",
+        keahlian: ["menembak jago", "tracking hewan", "survival liar", "strategi perampokan", "riding kuda"],
+        gambar: "image.png",
+        latar: "anggota geng Van der Linde, hidup outlaw di barat liar, sering kena dilema moral",
+        tantangan: "dikejar hukum, konflik internal geng, sakit TBC bikin drop",
+        pelajaran: "hidup ga cuma soal duit, pilihan nentuin akhir",
+        link: ""
+    },
+};
+
